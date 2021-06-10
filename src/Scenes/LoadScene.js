3eee89b2fc2d8f88334fd610fc1a767af656292e
Cesar ANDRIANIRINTSOA <cesar.andrianiritsnoa@oika.eu>
2021-06-10 15:52:17 +0200

    update route file

--- src/Scenes/LoadScene.js
+++ src/Scenes/LoadScene.js
@@ -12,36 +12,36 @@ export class LoadScene extends Phaser.Scene{
 
     preload(){
  // add logo image
- this.load.image("title_bg", "./assets/image/background-menu.png");
- this.load.image("end_bg", "./assets/image/backgroundend.png");
+ this.load.image("title_bg", "./dist/assets/image/background-menu.png");
+ this.load.image("end_bg", "./dist/assets/image/backgroundend.png");
 
- this.load.audio("title_music", "./assets/audio/menu-music.mp3");
- this.load.audio("end_music", "./assets/audio/failsound.mp3");
+ this.load.audio("title_music", "./dist/assets/audio/menu-music.mp3");
+ this.load.audio("end_music", "./dist/assets/audio/failsound.mp3");
 
- this.load.audio("soundbutton", "./assets/audio/screenbuttons.wav");
- this.load.image('sablier', './assets/image/time-01.svg');
- this.load.image('mort', './assets/image/mort.svg');
+ this.load.audio("soundbutton", "./dist/assets/audio/screenbuttons.wav");
+ this.load.image('sablier', './dist/assets/image/time-01.svg');
+ this.load.image('mort', './dist/assets/image/mort.svg');
     
- this.load.image('vitesse', './assets/image/vitesse.svg');
-//  this.load.image("options_button", "./assets/image/options_button1.png");
- this.load.image("play_button", "./assets/image/play_button1.png");
-//  this.load.image("logo", "./assets/image/logo.png");
- this.load.image("back_menu_button", "./assets/image/backtomenu.png");
- this.load.image("retry_button", "./assets/image/retry.png");
- this.load.spritesheet("cat", "./assets/sprite/cat.png",{
+ this.load.image('vitesse', './dist/assets/image/vitesse.svg');
+//  this.load.image("options_button", "./dist/assets/image/options_button1.png");
+ this.load.image("play_button", "./dist/assets/image/play_button1.png");
+//  this.load.image("logo", "./dist/assets/image/logo.png");
+ this.load.image("back_menu_button", "./dist/assets/image/backtomenu.png");
+ this.load.image("retry_button", "./dist/assets/image/retry.png");
+ this.load.spritesheet("cat", "./dist/assets/sprite/cat.png",{
      frameHeight: 32,
      frameWidth: 32
  });
 
- this.load.spritesheet("eric", "./assets/sprite/eric.png",{
+ this.load.spritesheet("eric", "./dist/assets/sprite/eric.png",{
     frameHeight: 32,
     frameWidth: 29.5
 });
 
-this.load.audio("title_music", "./assets/audio/shuinvy-childhood.mp3");
+this.load.audio("title_music", "./dist/assets/audio/shuinvy-childhood.mp3");
  
 // for(let i = 0; i < 100; i++){
-//     this.load.spritesheet("cat" + i++,"./assets/sprite/cat.png", {
+//     this.load.spritesheet("cat" + i++,"./dist/assets/sprite/cat.png", {
 //         frameWidth: 32,
 //         frameHeight: 32
 //     })
